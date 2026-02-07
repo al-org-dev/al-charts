33f1be26ea2d135ef05f82ae423d9328ae0b4113
Maruan Al-Shedivat <maruan@genesistherapeutics.ai>
2026-02-07 01:54:46 -0500

    Fix DocumentDrop compatibility in page option lookup

--- lib/al_charts.rb
+++ lib/al_charts.rb
@@ -30,9 +30,9 @@ module AlCharts
       baseurl = cfg['baseurl'] || ''
       out = []
 
-      if truthy?(page.dig('mermaid', 'enabled'))
+      if truthy?(nested_value(page, 'mermaid', 'enabled'))
         out << %(<script defer src="#{libs.dig('mermaid', 'url', 'js')}" integrity="#{libs.dig('mermaid', 'integrity', 'js')}" crossorigin="anonymous"></script>)
-        if truthy?(page.dig('mermaid', 'zoomable'))
+        if truthy?(nested_value(page, 'mermaid', 'zoomable'))
           out << %(<script defer src="#{libs.dig('d3', 'url', 'js')}" integrity="#{libs.dig('d3', 'integrity', 'js')}" crossorigin="anonymous"></script>)
         end
         out << %(<script defer src="#{baseurl}/assets/al_charts/js/mermaid-setup.js" type="text/javascript"></script>)
@@ -48,12 +48,12 @@ module AlCharts
         out << %(<script defer src="#{baseurl}/assets/al_charts/js/leaflet-setup.js" type="text/javascript"></script>)
       end
 
-      if truthy?(page.dig('chart', 'chartjs'))
+      if truthy?(nested_value(page, 'chart', 'chartjs'))
         out << %(<script defer src="#{libs.dig('chartjs', 'url', 'js')}" integrity="#{libs.dig('chartjs', 'integrity', 'js')}" crossorigin="anonymous"></script>)
         out << %(<script defer src="#{baseurl}/assets/al_charts/js/chartjs-setup.js" type="text/javascript"></script>)
       end
 
-      if truthy?(page.dig('chart', 'echarts'))
+      if truthy?(nested_value(page, 'chart', 'echarts'))
         out << %(<script src="#{libs.dig('echarts', 'url', 'js', 'library')}" integrity="#{libs.dig('echarts', 'integrity', 'js', 'library')}" crossorigin="anonymous"></script>)
         if truthy?(cfg['enable_darkmode'])
           out << %(<script src="#{libs.dig('echarts', 'url', 'js', 'dark_theme')}" integrity="#{libs.dig('echarts', 'integrity', 'js', 'dark_theme')}" crossorigin="anonymous"></script>)
@@ -61,12 +61,12 @@ module AlCharts
         out << %(<script defer src="#{baseurl}/assets/al_charts/js/echarts-setup.js" type="text/javascript"></script>)
       end
 
-      if truthy?(page.dig('chart', 'plotly'))
+      if truthy?(nested_value(page, 'chart', 'plotly'))
         out << %(<script defer src="#{libs.dig('plotly', 'url', 'js')}" integrity="#{libs.dig('plotly', 'integrity', 'js')}" crossorigin="anonymous"></script>)
         out << %(<script defer src="#{baseurl}/assets/al_charts/js/plotly-setup.js" type="text/javascript"></script>)
       end
 
-      if truthy?(page.dig('chart', 'vega_lite'))
+      if truthy?(nested_value(page, 'chart', 'vega_lite'))
         out << %(<script defer src="#{libs.dig('vega', 'url', 'js')}" integrity="#{libs.dig('vega', 'integrity', 'js')}" crossorigin="anonymous"></script>)
         out << %(<script defer src="#{libs.dig('vega-lite', 'url', 'js')}" integrity="#{libs.dig('vega-lite', 'integrity', 'js')}" crossorigin="anonymous"></script>)
         out << %(<script defer src="#{libs.dig('vega-embed', 'url', 'js')}" integrity="#{libs.dig('vega-embed', 'integrity', 'js')}" crossorigin="anonymous"></script>)
@@ -80,6 +80,16 @@ module AlCharts
 
     def truthy?(value)
       value == true || value.to_s == 'true'
+    end
+
+    def nested_value(container, *keys)
+      current = container
+      keys.each do |key|
+        return nil unless current.respond_to?(:[])
+
+        current = current[key]
+      end
+      current
     end
   end
 end
